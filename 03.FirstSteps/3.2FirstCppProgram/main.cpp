30799a0d4d48a8f67193cf1e426d6025c1dd5fab
Bob Patthammavong <vannakone.p@gmail.com>
2025-12-17 21:17:06 -0600

    first update code on c++

--- 03.FirstSteps/3.2FirstCppProgram/main.cpp
+++ 03.FirstSteps/3.2FirstCppProgram/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
+#include namespace std;
 int main(){
 
     std::cout << "Number1" << std::endl;
     std::cout << "Number2" << std::endl;
     std::cout << "Number3" << std::endl;
-
+    std
     return 0;
 }
